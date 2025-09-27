fabe082c2f780f41b1dfc48d589ebc94ab3c5f3f
64-HiroByte <higher.ground.rc@gmail.com>
2025-09-27 18:55:22 +0900

    ベースのHTMLを作成

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,44 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div>
+      <h1 className="text-3xl font-bold">Todo List</h1>
+      {/* 入力エリア */}
+      <div>
+        <h2>今日は何する？</h2>
+        <input className="border bg-green-50" type="text" />
+        <button>保存</button>
+      </div>
+      {/* Todoのリスト */}
+      <div>
+        {/* リスト */}
+        <ul>
+          <li>
+            <input type="checkbox" name="" id="" />
+            Todo１
+            <button>編集</button>
+            <button>削除</button>
+          </li>
+          <li>
+            <input type="checkbox" name="" id="" />
+            Todo２
+            <button>編集</button>
+            <button>削除</button>
+          </li>
+          <li>
+            <input type="checkbox" name="" id="" />
+            Todo３
+            <button>編集</button>
+            <button>削除</button>
+            <div></div>
+          </li>
+        </ul>
+        {/* リストの状態 */}
+        <footer>
+          <span>Todoアイテム数: XX, 完了済み: XX, 未完了: XX</span>
+        </footer>
+      </div>
+    </div>
   );
 }
 
